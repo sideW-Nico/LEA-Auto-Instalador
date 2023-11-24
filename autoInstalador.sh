507cd9af2120ae29afcd875de2321b1320c27fbc
Leandro <leandrolopezhegui@gmail.com>
2023-11-23 22:07:46 -0300

    Agregando funciones al auto instalador

--- autoInstalador.sh
+++ autoInstalador.sh
@@ -8,30 +8,38 @@ AZUL='\033[0;34m'
 ROJO='\033[0;31m'
 VERDE='\033[0;32m'
 
+function instalarPaquetes () {
+	if bash ./scripts/instalarPaquetes.sh ; then
+		echo "${VERDE}###Paquetes instalados exitosamente...${SIN_COLOR}"
+	else
+	echo "${ROJO}###Error al instalar los paquetes...${SIN_COLOR}"
+  		exit 1
+	fi
+}
+
+function configurarSSH () {
+	if bash ./scripts/configuracionSSH.sh ; then
+		echo "${VERDE}###SSH configurado correctamente...${SIN_COLOR}"
+	else
+		echo "${ROJO}###Error configurando SSH...${SIN_COLOR}"
+		exit 2
+	fi
+}
+
+function agregarArchivosDeConfiguracion () {
+	if bash ./scripts/archivosDeConfiguracion.sh ; then
+  		echo "${VERDE}###Archivos del sistema configurados correctamente...${SIN_COLOR}"
+	else
+  		echo "${VERDE}###Error configurando los archivos del sistema${SIN_COLOR}"
+  		exit 3
+	fi
+}
+
 echo "${AZUL}###AUTO INSTALADOR (por Leandro López)###${SIN_COLOR}"
 
-if bash ./scripts/instalarPaquetes.sh ; then
-  echo "${VERDE}###Paquetes instalados exitosamente...${SIN_COLOR}"
-else
-  echo "${ROJO}###Error al instalar los paquetes...${SIN_COLOR}"
-  exit 1
-fi
-
-
-
-if bash ./scripts/configuracionSSH.sh ; then
-  echo "${VERDE}###SSH configurado correctamente...${SIN_COLOR}"
-else
-  echo "${ROJO}###Error configurando SSH...${SIN_COLOR}"
-  exit 2
-fi
-
-if bash ./scripts/archivosDeConfiguracion.sh ; then
-  echo "${VERDE}###Archivos del sistema configurados correctamente...${SIN_COLOR}"
-else
-  echo "${VERDE}###Error configurando los archivos del sistema${SIN_COLOR}"
-fi
+instalarPaquetes
+configurarSSH
+agregarArchivosDeConfiguracion
 
 echo "${AZUL}###AUTO INSTALADOR FINALIZADO SIN ERRORES###${SIN_COLOR}"
-
 exit 0

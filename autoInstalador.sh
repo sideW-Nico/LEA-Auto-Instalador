fc4fb660fe25c6b2cee0df7068744a710df4dc63
Leandro <leandrolopezhegui@gmail.com>
2023-12-02 17:15:36 -0300

    Control de estados del instalador

--- autoInstalador.sh
+++ autoInstalador.sh
@@ -3,10 +3,22 @@
 #Útil para casos donde se necesita habilitar el manejo de caractéres especiales con echo y printf, dejando de necesitar el argumento -e
 shopt -s xpg_echo
 
+#Colores
 SIN_COLOR='\033[0m'
 AZUL='\033[0;34m'
 ROJO='\033[0;31m'
 VERDE='\033[0;32m'
+
+#Constantes
+readonly primeraInstalacion=0
+readonly paquetesInstalados=1
+readonly sshConfigurado=2
+readonly sistemaInstalado=-1
+
+#Estados del instalador, en este espacio se especifíca la ruta y el contenido del archivo,
+#los estados son identificados por los nombres de las cosntantes.
+rutaEstado="./estadosDelInstalador/estadoAutoInstalador.txt"
+let estadoActual=$(cat $rutaEstado)
 
 function instalarPaquetes () {
 	if bash ./scripts/instalarPaquetes.sh ; then
@@ -35,11 +47,53 @@ function agregarArchivosDeConfiguracion () {
 	fi
 }
 
-echo "${AZUL}###AUTO INSTALADOR (por Leandro López)###${SIN_COLOR}"
+function reanudarInstalacion () {
+	if [[ $estadoActual -gt 0 ]]; then
+		while : ; do
+			echo "${SIN_COLOR}Reanudar desde el último punto? [S/n]:"
+			read confirmacion
+			if [[ $confirmacion =~ ^[sS]$ ]] ; then
+				break
+			elif [[ $confirmacion =~ ^[nN]$ ]] ; then
+				echo "0" > $rutaEstado
+				finalizarInstalacion
+			else
+				echo "Debe ingresar {s,S}..."
+			fi
+		done
+	fi
+}
 
-instalarPaquetes
-configurarSSH
-agregarArchivosDeConfiguracion
+function modificarEstadoActual () {
+	echo $1 > $rutaEstado
+	$estadoActual=$(cat $rutaEstado)
+}
 
-echo "${AZUL}###AUTO INSTALADOR FINALIZADO SIN ERRORES###${SIN_COLOR}"
-exit 0
+function finalizarInstalacion (){
+	echo "${AZUL}###AUTO INSTALADOR FINALIZADO SIN ERRORES###${SIN_COLOR}"
+	exit 0
+}
+
+function main () {
+	echo "${AZUL}###AUTO INSTALADOR (por Leandro López)###${SIN_COLOR}"
+
+	reanudarInstalacion
+	if [[ $estadoActual -eq $primeraInstalacion ]] ; then
+		instalarPaquetes
+		modificarEstadoActual $paquetesInstalados
+	fi
+
+	if [[ $estadoActual -eq $paquetesInstalados ]] ; then
+		configurarSSH
+		modificarEstadoActual $sshConfigurado
+	fi
+
+	if [[ $estadoActual -eq $sshConfigurado ]] ; then
+		agregarArchivosDeConfiguracion
+		modificarEstadoActual $sistemaInstalado
+	fi
+
+	finalizarInstalacion
+}
+
+main
